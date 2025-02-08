0a4ddf9b1b34801219c7d491d207facab9932405
Speeroow <abhishek.rajawat1909@gmail.com>
2025-02-08 20:57:05 +0530

    first commit

---
+++ src/app/feed/[videoId]/page.tsx
@@ -0,0 +1,17 @@
+
+interface PageProps { 
+    params: Promise<{ videoId: string }>;
+}
+
+const Page = async ({ params }: PageProps) => {
+    
+    const {videoId}= await params;
+
+    return ( 
+        <div>
+            Video Id: {videoId}
+        </div>
+     );
+}
+ 
+export default Page;
